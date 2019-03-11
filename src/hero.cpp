31cd51709c31a498d62ff4486b26327810ea0b81
Andy <l1m0b@ugrad.cs.ubc.ca>
2019-03-10 22:17:39 -0700

    removed mesh.vao statement in destroy function in hero

--- src/hero.cpp
+++ src/hero.cpp
@@ -93,7 +93,6 @@ void Hero::destroy()
 {
 	glDeleteBuffers(1, &mesh.vbo);
 	glDeleteBuffers(1, &mesh.ibo);
-	glDeleteBuffers(1, &mesh.vao);
 
 	glDeleteShader(effect.vertex);
 	glDeleteShader(effect.fragment);
