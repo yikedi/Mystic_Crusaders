633d144ce5d074aa3439d6ef2bdc8f7c29252a9c
Andy <l1m0b@ugrad.cs.ubc.ca>
2019-03-10 22:05:15 -0700

    removed unnecessary subheight for hero spritesheet

--- src/hero.cpp
+++ src/hero.cpp
@@ -21,7 +21,6 @@ bool Hero::init(vec2 screen)
 	//std::vector<uint16_t> indices;
     hero_texture.totalTiles = 21; // custom to current sprite sheet
     hero_texture.subWidth = 64; // custom to current sprite sheet
-    hero_texture.subHeight = 64; // custom to current sprite sheet
     m_is_alive = false;
 
 	// Load shared texture
