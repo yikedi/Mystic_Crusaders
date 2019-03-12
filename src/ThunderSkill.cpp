bd1ef1a23e8cec34ce1c95617133977b19f21bd9
Shuoyi Ma <mashuoyi111@sina.com>
2019-03-11 23:50:12 -0700

    final balancing

--- src/ThunderSkill.cpp
+++ src/ThunderSkill.cpp
@@ -20,7 +20,7 @@ bool ThunderSkill::level_up(int select)
 			{
 				damage += 0.2f;
 				impactTime += 500;
-				mp_cost += 2.f;
+				mp_cost += 1.5f;
 				success = true;
 				damage_level++;
 			}
@@ -29,7 +29,7 @@ bool ThunderSkill::level_up(int select)
 			if (effect_level < 5)
 			{
 				scale = { scale.x * 1.15f , scale.y * 1.15f };
-				mp_cost += 2.f;
+				mp_cost += 1.5f;
 				success = true;
 				effect_level++;
 			}
