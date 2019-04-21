fc60e0a2beb9bc9d28e5585adae451c1c78004e5
咸臣 龙 <longxianchen@outlook.com>
2019-04-21 13:06:33 -0700

    fix q/e fix hero tree collision fix phoenix behind tree problem

--- src/world.cpp
+++ src/world.cpp
@@ -1419,8 +1419,6 @@ void World::draw()
 		e_proj.draw(projection_2D);
 	for (auto& thunder : thunders)
 		thunder->draw(projection_2D);
-	for (auto& phoenix : phoenix_list)
-		phoenix->draw(projection_2D);
 
 	if (start_is_over) {
 
@@ -1436,6 +1434,10 @@ void World::draw()
 		m_skill_switch.draw(projection_2D);
 		m_portal.draw(projection_2D);
 	}
+
+	//move the phoenix draw after treetrunk draw so that the phoenix is not behind the tree 
+	for (auto& phoenix : phoenix_list)
+		phoenix->draw(projection_2D);
 
 	if (game_is_paused){
 		stree.draw(projection_2D);
@@ -1748,14 +1750,14 @@ void World::on_key(GLFWwindow*, int key, int, int action, int mod)
 		game_is_paused = !game_is_paused;
 	}
 	else if (key == GLFW_KEY_E && action == GLFW_RELEASE) {
-		m_hero.set_active_skill(0);
+		int level_up_skill = (m_hero.get_active_skill() + 1) % 3;
+		m_hero.set_active_skill(level_up_skill);
 	}
 	else if (key == GLFW_KEY_Q && action == GLFW_RELEASE) {
-		m_hero.set_active_skill(1);
+		int level_up_skill = (m_hero.get_active_skill() - 1 + 3) % 3;
+		m_hero.set_active_skill(level_up_skill);
 	}
-	else if (key == GLFW_KEY_LEFT_SHIFT && action == GLFW_RELEASE) {
-		m_hero.set_active_skill(2);
-	}
+
 }
 
 void World::on_mouse_move(GLFWwindow* window, double xpos, double ypos)
